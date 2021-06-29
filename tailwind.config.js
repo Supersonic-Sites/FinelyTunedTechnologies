1a70b2b123b0e6358b6dfec0d61d9e8f8fa87c10
Renaud Gagne <dev@supersonicsites.com>
2021-06-29 08:58:26 -0400

    No fonts

--- tailwind.config.js
+++ tailwind.config.js
@@ -27,25 +27,7 @@ module.exports = {
   
   theme: {
     extend: {
-      
-      fontFamily: {
-        sans: [
-          '"Open Sans"',
-          'system-ui',
-          '-apple-system',
-          'BlinkMacSystemFont',
-          '"Segoe UI"',
-          'Roboto',
-          '"Helvetica Neue"',
-          'Arial',
-          '"Noto Sans"',
-          'sans-serif',
-          '"Apple Color Emoji"',
-          '"Segoe UI Emoji"',
-          '"Segoe UI Symbol"',
-          '"Noto Color Emoji"',
-        ],
-      },
+
       colors: {
         'primary': '#486078',
         
